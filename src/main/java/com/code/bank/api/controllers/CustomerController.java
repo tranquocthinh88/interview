cc1da2ddce6d11636a86d458ce44808e3eb89998
tranquocthinh88 <97369919+tranquocthinh88@users.noreply.github.com>
2025-02-28 17:02:39 +0700

    created, delete, update customer successfully

--- src/main/java/com/code/bank/api/controllers/CustomerController.java
+++ src/main/java/com/code/bank/api/controllers/CustomerController.java
@@ -3,6 +3,7 @@ package com.code.bank.api.controllers;
 import com.code.bank.api.dtos.requests.CustomerDto;
 import com.code.bank.api.dtos.responses.Response;
 import com.code.bank.api.dtos.responses.ResponseSuccess;
+import com.code.bank.api.exceptions.DataNotFoundException;
 import com.code.bank.api.mappers.CustomerMapper;
 import com.code.bank.models.Customer;
 import com.code.bank.services.interfaces.CustomerService;
@@ -10,10 +11,9 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping ("/api/v1/customers")
@@ -34,5 +34,30 @@ public class CustomerController {
         Customer customer = customerMapper.CustomerDto2Customer(customerDto);
         return new ResponseSuccess<>(HttpStatus.OK.value(), "create customer successfully",
                 customerService.save(customer));
+    }
+
+    @DeleteMapping("/{id}")
+    public Response deleteCustomer(@PathVariable int id) throws DataNotFoundException {
+        customerService.delete(id);
+        return new ResponseSuccess<>(HttpStatus.NO_CONTENT.value(),
+                "delete customer successfully" + id);
+    }
+
+    @PatchMapping("/{id}")
+    public Response patchCustomer(@PathVariable int id, @RequestBody @Valid Map<String, ?> data) throws Exception {
+        return new ResponseSuccess<>(
+                HttpStatus.OK.value(),
+                "address updated successfully",
+                customerService.updatePatch(id, data)
+        );
+    }
+
+    @PutMapping("/{id}")
+    public Response updateAddress(@PathVariable int id ,@RequestBody @Valid CustomerDto customerDto) throws Exception {
+        Customer customer = customerMapper.CustomerDto2Customer(customerDto);
+        customer.setId(id);
+        return new ResponseSuccess<>(HttpStatus.OK.value(),
+                "address updated successfully",
+                customerService.update(id, customer));
     }
 }
